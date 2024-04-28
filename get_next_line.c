a9cf409a4059fb22ff6a5385b16771ff8d166f5d
SuVao <pedro.radon@gmail.com>
2024-04-28 16:03:00 +0100

    get

--- get_next_line.c
+++ get_next_line.c
@@ -12,7 +12,7 @@
 
 #include "get_next_line.h"
 
-static char	*get_line(char *str)
+/*static char	*get_line(char *str)
 {
 	char	*line;
 	int		i;
@@ -42,6 +42,32 @@ char	*get_rest(char *s)
 	s2 = ft_substr(s, i, len);
 	free (s);
 	return (s2);
+}*/
+
+char	*get_nl(int fd, char *s)
+{
+	char	buffer[BUFFER_SIZE + 1];
+	int		bytes_read;
+	int		i;
+
+	i = 0;
+	while (i < BUFFER_SIZE)
+		buffer[i++] = '\0';
+	bytes_read = 1;
+	while (!ft_strchr(buffer, '\0') && bytes_read)
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read < 0 || (!bytes_read && !s))
+			return (NULL);
+		buffer[bytes_read] = '\0';
+		if (!s)
+			s = ft_strdup(buffer);
+		else
+			s = ft_strjoin(s, buffer);
+		if (!s)
+			return (NULL);
+	}
+	return (s);
 }
 
 char	*get_next_line(int fd)
@@ -51,26 +77,22 @@ char	*get_next_line(int fd)
 	char		*rest_of_chars;
 	int			i;
 
-	if (fd < 0 || BUFFER_SIZE < 1)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	ori_str = malloc(sizeof(char *) * (BUFFER_SIZE + 1));
+	ori_str = get_nl(fd, ori_str);
 	if (!ori_str)
 		return (NULL);
-	i = 1;
-	while (!ft_strchr(ori_str, '\n') && i > 0)
-	{
-		i = read(fd, ori_str, BUFFER_SIZE);
-		if (i < 0)
-		{
-			free (ori_str);
-			return (NULL);
-		}
-		ori_str[BUFFER_SIZE] = '\0';
-		final_boss = get_line(ori_str);
-		rest_of_chars = get_rest(ori_str);
-		final_boss = ft_strjoin(final_boss, rest_of_chars);
-		free (rest_of_chars);
-	}
+	i = 0;
+	while (ori_str[i] != '\0' && ori_str[i])
+		i++;
+	if (ori_str[i] == '\n')
+		i++;
+	final_boss = ft_substr(ori_str, 0, i);
+	printf("final boss: %s \n", final_boss);
+	rest_of_chars = ft_substr(ori_str, i, ft_strlen(ori_str) - i);
+	free (ori_str);
+	ori_str = rest_of_chars;
+	printf("str: %s \n", final_boss);
 	return (final_boss);
 }
 
@@ -115,7 +137,7 @@ int	main()
 	char	*line;
 
 	fd = open("test.txt", O_RDONLY);
-	while ((line = get_next_line(fd)) != NULL)
+	while ((line = get_next_line(fd)))
 		printf("linha: %s \n", line++);
 	close(fd);
 	return (0);
